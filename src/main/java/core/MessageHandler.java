13e8558664081b291831b5a4766f59cf7eb769d5
Hax <arzberger52@gmail.com>
2018-02-17 16:51:46 +0100

    Fixed a Problem with MySql

--- src/main/java/core/MessageHandler.java
+++ src/main/java/core/MessageHandler.java
@@ -20,26 +20,28 @@ public class MessageHandler {
             Connection con = DriverManager.getConnection(url + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", SECRETS.user, SECRETS.password);
             PreparedStatement pst = con.prepareStatement("SELECT * FROM `server` WHERE ID='"+guild.getId()+"'");
             ResultSet rs = pst.executeQuery();
-            //lang
-            if (message.equals("lang")) {
-                if (lang.equals("english")) {
-                    Titel = "Your language";
-                    Message = "Your language is english\n" +
-                            "You can edit your language with "+rs.getString(2)+"language <german|english>";
-                } else if (lang.equals("german")) {
-                    Titel = "Deine Sprache";
-                    Message = "Deine Sprache ist derzeit German\n" +
-                            "Umstellen kannst du sie mit "+rs.getString(2)+"language <german|english>";
+            if (rs.next()) {
+                //lang
+                if (message.equals("lang")) {
+                    if (lang.equals("english")) {
+                        Titel = "Your language";
+                        Message = "Your language is english\n" +
+                                "You can edit your language with " + rs.getString(2) + "language <german|english>";
+                    } else if (lang.equals("german")) {
+                        Titel = "Deine Sprache";
+                        Message = "Deine Sprache ist derzeit German\n" +
+                                "Umstellen kannst du sie mit " + rs.getString(2) + "language <german|english>";
+                    }
                 }
-            }
-            // Langedit
-            if (message.equals("langedit")) {
-                if (lang.equals("english")) {
-                    Titel= "Your language was edited";
-                    Message="Your language is now english!";
-                } else if (lang.equals("german")) {
-                    Titel="Deine Sprache wurde geändert";
-                    Message="Deine Sprache ist nun Deutsch!";
+                // Langedit
+                if (message.equals("langedit")) {
+                    if (lang.equals("english")) {
+                        Titel = "Your language was edited";
+                        Message = "Your language is now english!";
+                    } else if (lang.equals("german")) {
+                        Titel = "Deine Sprache wurde geändert";
+                        Message = "Deine Sprache ist nun Deutsch!";
+                    }
                 }
             }
         } catch (Exception e) {
