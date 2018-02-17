8ca5836fd08f157b98a39ffd5dc0675b50884e97
Hax <arzberger52@gmail.com>
2018-02-17 13:06:44 +0100

    Update Main

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -1,9 +1,12 @@
 package core;
 
 import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.OnlineStatus;
 import stuff.SECRETS;
+
+import javax.security.auth.login.LoginException;
 
 public class Main {
 
@@ -11,6 +14,13 @@ public class Main {
 
     public static void main(String[] args) {
         builder = new JDABuilder(AccountType.BOT).setToken(SECRETS.TOKEN).setAutoReconnect(true).setStatus(OnlineStatus.ONLINE);
+
+
+        try {
+            JDA jda = builder.buildBlocking();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
