f10103c03e6f24e6581c69f3c29a7969a7e0fd66
Hax <arzberger52@gmail.com>
2018-02-17 14:35:37 +0100

    Update Guildjoin

--- src/main/java/listener/Guildjoin.java
+++ src/main/java/listener/Guildjoin.java
@@ -15,7 +15,7 @@ public class Guildjoin extends ListenerAdapter {
 
     public void onGuildJoin(GuildJoinEvent event) {
         try {
-            System.out.println("Ich bin auf einem neuen Server: Name: "+event.getGuild().getName()+" ID: "+event.getGuild().getId()+" User: "+event.getGuild().getMembers().size());
+            System.out.println("Ich bin auf einem neuen Server: Name: "+event.getGuild().getName()+" ID: "+event.getGuild().getId()+" Member: "+event.getGuild().getMembers().size());
             Connection con = DriverManager.getConnection(urlempty + "miner?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", SECRETS.user, SECRETS.password);
             PreparedStatement pst = con.prepareStatement("Select * FROM `ID` WHERE ID=" + event.getGuild().getId());
             ResultSet rs = pst.executeQuery();
