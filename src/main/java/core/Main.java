ad472e78e629f14199317ce09da4aec5dd09440b
GregTCLTK <gregtcltk@gmail.com>
2018-02-27 20:17:06 +0100

    x

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -34,6 +34,7 @@ public class Main {
         commandHandler.commands.put("profile", new profile());
         commandHandler.commands.put("clan", new Clan());
         commandHandler.commands.put("givehashes", new givehashes());
+        commandHandler.commands.put("guilds", new guilds());
         logger.info("loaded all commands");
         try {
             JDA jda = builder.buildBlocking();
