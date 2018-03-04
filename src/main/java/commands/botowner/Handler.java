1e6ddb3442cd964cfc66d65fa4d8d663320838ee
GregTCLTK <gregtcltk@gmail.com>
2018-03-04 18:27:40 +0100

    EZZ

--- src/main/java/commands/botowner/Handler.java
+++ src/main/java/commands/botowner/Handler.java
@@ -6,11 +6,9 @@ public class Handler {
 
     public static Boolean get(User user) {
         boolean yes=false;
-        System.out.println("Bruh");
         if (user.getId().equals("362270177712275491")||user.getId().equals("261083609148948488")) {
-            System.out.println("Ownaa!");
             yes=true;
-        } else System.out.println("nope");
+        } else System.out.println("keine perm's");
         return yes;
     }
 
