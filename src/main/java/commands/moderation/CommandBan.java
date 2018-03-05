db36ea031c737baf74285f667d36029a00c25c10
GregTCLTK <gregtcltk@gmail.com>
2018-03-05 20:13:55 +0100

    Ban Command

--- src/main/java/commands/moderation/CommandBan.java
+++ src/main/java/commands/moderation/CommandBan.java
@@ -1,4 +1,50 @@
 package commands.moderation;
 
-public class CommandBan {
+import commands.Command;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.PrivateChannel;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+import java.awt.*;
+
+public class CommandBan implements Command {
+    @Override
+    public boolean called(String[] args, MessageReceivedEvent event) {
+        return false;
+    }
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent event) {
+        Message msg = event.getMessage();
+        if (msg.getMentionedUsers().isEmpty()) {
+            event.getTextChannel().sendMessage(new EmbedBuilder().setDescription("gb.ban <@User>")
+                    .setTitle("Usage").setColor(Color.MAGENTA).build()).queue();
+        }
+        Member target = msg.getGuild().getMember(msg.getMentionedUsers().get(0));
+        if (!msg.getGuild().getSelfMember().canInteract(target)) {
+            event.getTextChannel().sendMessage(new EmbedBuilder().setDescription("Sorry I can't ban this User.")
+                    .setTitle(":warning: No permissions").setColor(Color.MAGENTA).build()).queue();
+        } else {
+            if (!target.getUser().isBot()) {
+                PrivateChannel channel = target.getUser().openPrivateChannel().complete();
+                channel.sendMessage(new EmbedBuilder().setDescription("You got banned")
+                        .setTitle(":white_check_mark: Banned").setColor(Color.MAGENTA).build()).queue();
+            }
+            msg.getGuild().getController().ban(target, 7).queue();
+            event.getTextChannel().sendMessage(new EmbedBuilder().setDescription("Succesfully banned")
+                    .setTitle(":white_check_mark: Banned").setColor(Color.MAGENTA).build()).queue();
+        }
+    }
+
+        @Override
+    public void executed(boolean success, MessageReceivedEvent event) {
+
+    }
+
+    @Override
+    public String help() {
+        return null;
+    }
 }
