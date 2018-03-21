da3585a01f81ba1c708651d7d9867c6115697401
GregTCLTK <gregtcltk@gmail.com>
2018-03-21 13:54:58 +0100

    FAQ Command

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -3,6 +3,7 @@ package core;
 import commands.botowner.*;
 import commands.games.CommandWork;
 import commands.moderation.*;
+import commands.tools.CommandFAQ;
 import commands.tools.CommandGitHub;
 import commands.tools.CommandPing;
 import commands.tools.CommandProfile;
@@ -62,6 +63,7 @@ public class Main {
         commandHandler.commands.put("blacklist", new CommandBlacklist());
         commandHandler.commands.put("guilds", new CommandGuilds());
         commandHandler.commands.put("lvlmessage", new CommandLevelMessage());
+        commandHandler.commands.put("faq", new CommandFAQ());
         // commandHandler.commands.put("warn", new CommandWarn());
         logger.info("loaded all commands");
         try {
