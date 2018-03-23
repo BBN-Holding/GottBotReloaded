654d255198bb334419e8764b33544a5e8509ba31
GregTCLTK <gregtcltk@gmail.com>
2018-03-23 19:38:11 +0100

    HYPE

--- src/main/java/stuff/DATA.java
+++ src/main/java/stuff/DATA.java
@@ -1,8 +1,10 @@
 package stuff;
 
+import net.dv8tion.jda.core.entities.Guild;
+
 public class DATA {
     public static String url="jdbc:mysql://localhost/gottbot";
-    public static String BBNS = "396732579920740352";
+    public static Guild BBNS = "396732579920740352";
     public static String Premium = "408660274103451649";
 
 }
