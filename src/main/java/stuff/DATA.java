6cd485518b0c571c80c46c85c0cc8d2fcf4f1a4b
GregTCLTK <gregtcltk@gmail.com>
2018-03-23 19:52:48 +0100

    :kms2:

--- src/main/java/stuff/DATA.java
+++ src/main/java/stuff/DATA.java
@@ -4,7 +4,7 @@ import net.dv8tion.jda.core.entities.Guild;
 
 public class DATA {
     public static String url="jdbc:mysql://localhost/gottbot";
-    public static Guild BBNS = "396732579920740352";
+    public static String BBNS = "396732579920740352";
     public static String Premium = "408660274103451649";
 
 }
