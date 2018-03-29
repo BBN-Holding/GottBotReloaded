66a2f4c1bd2e211c1af73f3acff4425e5dd21752
Hax <arzberger52@gmail.com>
2018-03-29 10:26:17 +0200

    Restart weg work weg help geupdated,...

--- src/main/java/listener/PrivateMessage.java
+++ src/main/java/listener/PrivateMessage.java
@@ -11,18 +11,19 @@ public class PrivateMessage extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
+        if (!event.getAuthor().isBot()) {
+            if (event.getChannelType().equals(ChannelType.PRIVATE)) {
 
-        if (event.getChannelType().equals(ChannelType.PRIVATE)) {
+                String Content = event.getMessage().getContentDisplay();
+                String Author = event.getAuthor().getName() + "#" + event.getAuthor().getDiscriminator();
 
-            String Content = event.getMessage().getContentDisplay();
-            String Author = event.getAuthor().getName() + "#" + event.getAuthor().getDiscriminator();
+                PrivateChannel Hax = event.getJDA().getUserById(DATA.Hax).openPrivateChannel().complete();
+                PrivateChannel Skidder = event.getJDA().getUserById(DATA.Skidder).openPrivateChannel().complete();
 
-            PrivateChannel Hax = event.getJDA().getUserById(DATA.Hax).openPrivateChannel().complete();
-            PrivateChannel Skidder = event.getJDA().getUserById(DATA.Skidder).openPrivateChannel().complete();
+                Hax.sendMessage(new EmbedBuilder().setTitle("Neue Private Nachricht von " + Author + " .").setDescription(Content).build()).queue();
+                Skidder.sendMessage(new EmbedBuilder().setTitle("Neue Private Nachricht von " + Author + " .").setDescription(Content).build()).queue();
 
-            Hax.sendMessage(new EmbedBuilder().setTitle("Neue Private Nachricht von " + Author + " .").setDescription(Content).build()).queue();
-            Skidder.sendMessage(new EmbedBuilder().setTitle("Neue Private Nachricht von " + Author + " .").setDescription(Content).build()).queue();
-
+            }
         }
     }
 
