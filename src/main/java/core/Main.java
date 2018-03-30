6e47c6e2b69b5db5d1c261a243dc3ed690069380
GregTCLTK <gregtcltk@gmail.com>
2018-03-30 23:07:45 +0200

    so

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -1,55 +1,64 @@
 package core;
 
 import commands.botowner.*;
-import commands.games.CommandWork;
 import commands.moderation.*;
 import commands.tools.CommandGitHub;
 import commands.tools.CommandPing;
 import commands.tools.CommandProfile;
 import commands.tools.CommandToken;
 import commands.usercommands.*;
+import commands.usercommands.CommandHelp3;
+import commands.usercommands.CommandInfo;
 import listener.*;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.OnlineStatus;
 import net.dv8tion.jda.core.entities.Game;
+import org.apache.commons.net.ftp.FTPClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import stuff.SECRETS;
 
 import java.io.File;
+import java.io.FileInputStream;
 
 public class Main {
     public static JDABuilder builder;
     private static Logger logger = LoggerFactory.getLogger(Main.class);
     public static JDA jda;
     public static String[] args;
-    static boolean dev=false;
 
     public static void main(String[] args2) {
         try {
             if (!new File("Gott.log").exists()) {
                 new File("Gott.log").createNewFile();
             }
+            FTPClient client = new FTPClient();
+            FileInputStream fis = null;
+            client.connect("ftp.bigbotnetwork.de");
+            client.login(SECRETS.FTPUSER, SECRETS.FTPPW);
+            String filename = "Gott.log";
+            fis = new FileInputStream(filename);
+            client.storeFile(filename, fis);
+            client.logout();
             logger.info("------------------start Bot----------------------");
             logger.info("read Token and logins");
             MySQL.connect();
-            builder = new JDABuilder(AccountType.BOT).setToken(SECRETS.TOKEN).setAutoReconnect(true);
-            builder.setGame(Game.playing("Starting...."));
-            builder.setStatus(OnlineStatus.DO_NOT_DISTURB);
+            builder = new JDABuilder(AccountType.BOT).setToken(SECRETS.TOKEN).setAutoReconnect(true).setStatus(OnlineStatus.ONLINE).setGame(Game.streaming("@GottBot", "https://www.twitch.tv/bigbotnetwork"));
 
             builder.addEventListener(new commandListener());
             builder.addEventListener(new Guildjoin());
             builder.addEventListener(new Message());
             builder.addEventListener(new Memberjoin());
             builder.addEventListener(new Reaction());
-            builder.addEventListener(new BotList());
+            builder.addEventListener(new PrivateMessage());
+            builder.addEventListener(new LogListener());
+            // builder.addEventListener(new BotList());
             logger.info("loaded all listeners");
             commandHandler.commands.put("language", new CommandLanguage());
             commandHandler.commands.put("test", new CommandTest());
             commandHandler.commands.put("prefix", new CommandPrefix());
-            commandHandler.commands.put("help", new CommandHelp());
             commandHandler.commands.put("bug", new CommandBug());
             commandHandler.commands.put("profile", new CommandProfile());
             commandHandler.commands.put("givehashes", new CommandGiveHashes());
@@ -63,10 +72,9 @@ public class Main {
             commandHandler.commands.put("stop", new CommandStop());
             commandHandler.commands.put("setlvl", new CommandSetLevel());
             commandHandler.commands.put("setxp", new CommandSetXP());
-            commandHandler.commands.put("work", new CommandWork());
             commandHandler.commands.put("clyde", new CommandClyde());
             commandHandler.commands.put("ping", new CommandPing());
-            commandHandler.commands.put("guildleave", new CommandGuildLeave());
+            commandHandler.commands.put("leave", new CommandGuildLeave());
             commandHandler.commands.put("stats", new CommandStats());
             commandHandler.commands.put("verification", new CommandVerification());
             commandHandler.commands.put("say", new CommandSay());
@@ -74,28 +82,24 @@ public class Main {
             commandHandler.commands.put("guilds", new CommandGuilds());
             commandHandler.commands.put("lvlmessage", new CommandLevelMessage());
             commandHandler.commands.put("guild", new CommandGuild());
-            commandHandler.commands.put("help2", new CommandHelp2());
-            commandHandler.commands.put("help3", new CommandHelp3());
+            commandHandler.commands.put("help", new CommandHelp3());
             commandHandler.commands.put("info", new CommandInfo());
-            commandHandler.commands.put("restart", new CommandRestart());
             commandHandler.commands.put("warn", new CommandWarn());
             commandHandler.commands.put("token", new CommandToken());
             commandHandler.commands.put("log", new CommandLog());
             commandHandler.commands.put("play", new CommandPlay());
             commandHandler.commands.put("dm", new CommandDM());
-            commandHandler.commands.put("role", new CommandRole());
-
-
+            commandHandler.commands.put("webhook", new CommandWebHook());
+            commandHandler.commands.put("miner", new CommandMiner());
             args = args2;
             logger.info("loaded all commands");
-            if (!dev) {
-                logger.error("dev Mode - don't load the botlist stats");
-                builder.removeEventListener(new BotList());
-            }
             jda = builder.buildBlocking();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+
     }
+
 }
