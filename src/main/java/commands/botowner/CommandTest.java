dad357a98aa6edf7482c3e8b9d2a0e3b4b8b311a
Greg TK <33130539+GregTCLTK@users.noreply.github.com>
2018-03-30 22:48:40 +0200

    Update CommandTest.java

--- src/main/java/commands/botowner/CommandTest.java
+++ src/main/java/commands/botowner/CommandTest.java
@@ -1,22 +1,11 @@
 package commands.botowner;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import commands.Command;
 import core.MessageHandler;
-import de.foryasee.httprequest.HttpRequestBuilder;
-import de.foryasee.httprequest.RequestHeader;
-import de.foryasee.httprequest.RequestResponse;
-import de.foryasee.httprequest.RequestType;
-import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import util.Embed;
 
-import java.io.IOException;
-import java.util.Arrays;
 
 public class CommandTest implements Command {
 
@@ -30,9 +19,13 @@ public class CommandTest implements Command {
         if (Owner.get(event.getAuthor())) {
            new MessageBuilder().setEmbed(Embed.normal(MessageHandler.get(event.getAuthor()).getString("testtitel"), MessageHandler.get(event.getAuthor()).getString("testtext")).build()).build();
 
+
+
         } else {
             new MessageBuilder().setEmbed(Embed.error(MessageHandler.get(event.getAuthor()).getString("nopermstitel"), MessageHandler.get(event.getAuthor()).getString("nopermstext")).build()).build();
         }
+
+
     }
 
     @Override
