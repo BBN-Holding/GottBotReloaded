7d32c32131d4298a6ea2c4c7d9d131039365d71a
Greg TK <33130539+GregTCLTK@users.noreply.github.com>
2018-03-30 20:23:58 +0200

    Update CommandTest.java

--- src/main/java/commands/botowner/CommandTest.java
+++ src/main/java/commands/botowner/CommandTest.java
@@ -1,12 +1,25 @@
 package commands.botowner;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import commands.Command;
 import core.MessageHandler;
+import de.foryasee.httprequest.HttpRequestBuilder;
+import de.foryasee.httprequest.RequestHeader;
+import de.foryasee.httprequest.RequestResponse;
+import de.foryasee.httprequest.RequestType;
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import util.Embed;
 
+import java.io.IOException;
+import java.util.Arrays;
 
 public class CommandTest implements Command {
+
     @Override
     public boolean called(String[] args, MessageReceivedEvent event) {
         return false;
@@ -15,11 +28,15 @@ public class CommandTest implements Command {
     @Override
     public void action(String[] args, MessageReceivedEvent event) {
         if (Owner.get(event.getAuthor())) {
-            event.getTextChannel().sendMessage(new EmbedBuilder().setTitle(MessageHandler.get(event.getAuthor()).getString("testtitel"))
-                    .setDescription(MessageHandler.get(event.getAuthor()).getString("testtext")).build()).queue(); // print wenn user CommandLanguage is english Test english wen user CommandLanguage is german Test german
+           new MessageBuilder().setEmbed(Embed.normal(MessageHandler.get(event.getAuthor()).getString("testtitel"), MessageHandler.get(event.getAuthor()).getString("testtext")).build()).build();
 
-        } else event.getTextChannel().sendMessage(new EmbedBuilder().setTitle(MessageHandler.get(event.getAuthor()).getString("nopermstitel"))
-                .setDescription(MessageHandler.get(event.getAuthor()).getString("nopermstext")).build()).queue();
+            HttpRequestBuilder httpRequestBuilder = new HttpRequestBuilder("https://botlist.space/api/bots/407189087649398795", RequestType.GET)
+
+
+        } else {
+            new MessageBuilder().setEmbed(Embed.error(MessageHandler.get(event.getAuthor()).getString("nopermstitel"), MessageHandler.get(event.getAuthor()).getString("nopermstext")).build()).build();
+        }
+
 
     }
 
