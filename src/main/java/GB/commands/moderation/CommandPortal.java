ed2143d2c53eb76b62563ef42e608ac28ff80fb3
Hax <arzberger52@gmail.com>
2018-06-09 22:36:19 +0200

    clean portal

--- src/main/java/GB/commands/moderation/CommandPortal.java
+++ src/main/java/GB/commands/moderation/CommandPortal.java
@@ -9,91 +9,15 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 public class CommandPortal implements Command {
     @Override
     public String[] Aliases() {
-        return new String[]{"portal"};
+        return new String[0];
     }
 
     @Override
     public void action(String[] args, MessageReceivedEvent event) {
-        /*
-
-        TODO: add portal table
-        TODO: translate
-        TODO: invite system
-
-        */
         if (args.length<1) {
-            event.getTextChannel().sendMessage(GottBot.getMessage().getCommandTemplate(Aliases(), "gb.portal open|info|connect|close|public", "Opens a portal to another Server")).queue();
+            event.getTextChannel().sendMessage(GottBot.getMessage().getCommandTemplate(Aliases(), "", "")).queue();
         } else {
-            switch (args[0].toLowerCase()) {
-                case "open":
-                            // adding to DB
-                            if (GottBot.getDB().get("portal", "serverid", event.getGuild().getId(), "connected") == null) {
-                                GottBot.getDB().insertPortal(event.getGuild().getId());
-                            }
-                            // create Channel if not exist
-                            if (GottBot.getDB().get("portal", "serverid", event.getGuild().getId(), "channel").equals("none")) {
-                                String portalchannel = event.getGuild().getController().createTextChannel("gb-portal").complete().getId();
-                                GottBot.getDB().update("portal", "serverid",event.getGuild().getId(),
-                                        GottBot.getDB().getR().hashMap("channel", portalchannel));
-                            }
-                            // set connected true
-                            GottBot.getDB().update("portal", "serverid", event.getGuild().getId(), GottBot.getDB().getR().hashMap("connected", "true"));
-                            event.getTextChannel().sendMessage("opened portal").queue();
-                    break;
-                case "public":
-                    if (GottBot.getDB().get("portal", "serverid", event.getGuild().getId(), "connected").equals("true")) {
-                        String edit;
-                        if (GottBot.getDB().get("portal", "serverid", event.getGuild().getId(), "public").equals("true")) {
-                            edit="false";
-                        } else edit="true";
-                        GottBot.getDB().update("portal", "serverid", event.getGuild().getId(),
-                                GottBot.getDB().getR().hashMap("public", edit));
-                        event.getTextChannel().sendMessage("set publicy to "+edit).queue();
-                    } else event.getTextChannel().sendMessage("first connect with gb.portal open").queue();
-                    break;
-                case "info":
-                    if (GottBot.getDB().get("portal", "serverid", event.getGuild().getId(), "connected").equals("true")) {
-                        String out = "";
-                        String connectedServer=GottBot.getDB().get("portal", "serverid", event.getGuild().getId(), "connectedservers");
-                        String[] servers=connectedServer.split(":");
-                        for (String server:servers) {
-                            Guild guild = GottBot.getShardManager().getGuildById(server);
-                            out+=guild.getName()+" ("+guild.getId()+")  ";
-                        }
-                        event.getTextChannel().sendMessage(new EmbedBuilder().setTitle("Portal - Info")
-                                .addField("connectedportals", out, false)
-                                .build()
-                        ).queue();
-                    } else event.getTextChannel().sendMessage("first connect with gb.portal open").queue();
-                    break;
-                case "connect":
-                    if (args.length == 2) {
-                        if (GottBot.getShardManager().getGuildById(args[1]) != null) {
-                            if (GottBot.getDB().get("portal", "serverid", event.getGuild().getId(), "connected").equals("true")) {
-                                if (GottBot.getDB().get("portal", "serverid", event.getGuild().getId(), "public").equals("true")) {
-                                    String id=event.getGuild().getId();
-                                    String id2=args[1];
-                                    for (int i =0; 2>i; i++) {
-                                        GottBot.getDB().update("portal", "serverid", id,
-                                                GottBot.getDB().getR()
-                                                        .hashMap("connectedservers",
-                                                                GottBot.getDB().get("portal", "serverid", id, "connectedservers")
-                                                                        + id2+":"));
-                                        String temp = id;
-                                        id=id2;
-                                        id2=temp;
-                                    }
-                                    event.getJDA().getTextChannelById(
-                                            GottBot.getDB().get("portal", "serverid", event.getGuild().getId(), "channel"))
-                                            .sendMessage("connected to server ``"+GottBot.getShardManager().getGuildById(args[1]).getName()+"``").queue();
-                                    GottBot.getShardManager().getTextChannelById(GottBot.getDB().get("portal", "serverid", args[1], "channel"))
-                                            .sendMessage("server  ``"+event.getGuild().getName()+"`` joined").queue();
-                                } else event.getTextChannel().sendMessage("you can only join with an invite...").queue();
-                            } else event.getTextChannel().sendMessage("first open connection with gb.portal open").queue();
-                        } else event.getTextChannel().sendMessage("This Guild not exist").queue();
-                    } else event.getTextChannel().sendMessage("look in the help :facepalm:").queue();
-                    break;
-            }
+
         }
     }
 }
--- src/main/java/GB/listener/shardmanagerlistener.java
+++ src/main/java/GB/listener/shardmanagerlistener.java
@@ -6,30 +6,20 @@ import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.Webhook;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import net.dv8tion.jda.webhook.WebhookClient;
+import net.dv8tion.jda.webhook.WebhookClientBuilder;
+import net.dv8tion.jda.webhook.WebhookMessage;
+import net.dv8tion.jda.webhook.WebhookMessageBuilder;
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 public class shardmanagerlistener extends ListenerAdapter {
+    static ArrayList<TextChannel> textChannels = new ArrayList<>();
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         if (!event.getAuthor().isBot()) {
-            if (GottBot.getDB().get("portal", "serverid", event.getGuild().getId(), "connected").equals("true")) {
-                if (GottBot.getDB().get("portal", "serverid", event.getGuild().getId(), "channel").equals(event.getTextChannel().getId())) {
-                    String guildsconnected=GottBot.getDB().get("portal", "serverid", event.getGuild().getId(), "connectedservers");
-                    String[] guilds = guildsconnected.split(":");
-                    ArrayList<TextChannel> textChannels = new ArrayList<TextChannel>();
-                    for (String guild:guilds) {
-                        String channel = GottBot.getDB().get("portal", "serverid", guild, "channel");
-                        textChannels.add(event.getJDA().getTextChannelById(channel));
-                    }
-                    for (TextChannel channel:textChannels) {
-                        // TODO: Add Avatar
-                        Webhook webhook = channel.createWebhook(event.getAuthor().getName()+" ("+event.getGuild().getName()+")").complete();
-                        webhook.getChannel().sendMessage(event.getMessage().getContentRaw()).queue();
-                        webhook.delete().queue();
-                    }
-                }
-            }
+
         }
     }
 }
